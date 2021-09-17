802fd4125c26978e76715309ac88e0c6a9a061cb
jsu <jsu317@gmail.com>
2021-09-17 10:16:30 -0500

    revert __init__.py

---
+++ airflow_prometheus_exporter/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Airflow Prometheus Exporter."""
+
+__author__ = """Robinhood Markets, Inc."""
+__email__ = "open-source@robinhood.com"
+__version__ = "__version__ = '1.0.7'"
