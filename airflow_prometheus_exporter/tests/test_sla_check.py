99e574535c6094e75d069e827b2b2a88f8585b48
jsu <jsu317@gmail.com>
2021-09-23 10:26:04 -0500

    adding comments; grouping

--- airflow_prometheus_exporter/tests/test_sla_check.py
+++ airflow_prometheus_exporter/tests/test_sla_check.py
@@ -11,214 +11,422 @@ def hours_from_now(hours):
 
 
 def sla_time(hours):
-    return hours_from_now(hours).astimezone(tz.gettz("America/Los_Angeles")).strftime("%H:%M")
+    return (
+        hours_from_now(hours)
+        .astimezone(tz.gettz("America/Los_Angeles"))
+        .strftime("%H:%M")
+    )
 
 
 @pytest.mark.parametrize(
     "sla_check_kwargs, expected_result",
     [
+        # Group 1:
+        #    - After SLA time or SLA time is None
+        #    - return max_execution_date < checkpoint
         (
+            # 0. max_execution_date -> checkpoint -> now
             {
-                "sla_interval": "1h",
+                "sla_interval": "2h",
                 "sla_time": sla_time(-1),
-                "max_execution_date": hours_from_now(-3),
-                "cadence": "triggered",
-                "latest_sla_miss_state": True
+                "max_execution_date": hours_from_now(-4),
+                "cadence": "intraday",
+                "latest_sla_miss_state": True,
             },
             True,
         ),
         (
+            # 1. max_execution_date -> checkpoint -> now
             {
-                "sla_interval": "1h",
+                "sla_interval": "2h",
                 "sla_time": sla_time(-1),
-                "max_execution_date": hours_from_now(-3),
-                "cadence": "triggered",
-                "latest_sla_miss_state": False
+                "max_execution_date": hours_from_now(-4),
+                "cadence": "intraday",
+                "latest_sla_miss_state": False,
             },
             True,
         ),
         (
+            # 2. max_execution_date -> checkpoint -> now
             {
-                "sla_interval": "1h",
+                "sla_interval": "2h",
                 "sla_time": sla_time(-1),
-                "max_execution_date": hours_from_now(-3),
+                "max_execution_date": hours_from_now(-4),
                 "cadence": "intraday",
-                "latest_sla_miss_state": True
+                "latest_sla_miss_state": None,
             },
             True,
         ),
         (
+            # 3. max_execution_date -> checkpoint -> now
             {
-                "sla_interval": "1h",
+                "sla_interval": "2h",
                 "sla_time": sla_time(-1),
-                "max_execution_date": hours_from_now(-3),
-                "cadence": "intraday",
-                "latest_sla_miss_state": False
+                "max_execution_date": hours_from_now(-4),
+                "cadence": "triggered",
+                "latest_sla_miss_state": True,
             },
             True,
         ),
         (
+            # 4. max_execution_date -> checkpoint -> now
             {
-                "sla_interval": "3h",
+                "sla_interval": "2h",
                 "sla_time": sla_time(-1),
-                "max_execution_date": hours_from_now(-3),
+                "max_execution_date": hours_from_now(-4),
                 "cadence": "triggered",
-                "latest_sla_miss_state": True
-            },
-            False,
-        ),
-        (
-            {
-                "sla_interval": "3h",
-                "sla_time": sla_time(-1),
-                "max_execution_date": hours_from_now(-3),
-                "cadence": "triggered",
-                "latest_sla_miss_state": False
-            },
-            False,
-        ),
-        (
-            {
-                "sla_interval": "3h",
-                "sla_time": sla_time(-1),
-                "max_execution_date": hours_from_now(-3),
-                "cadence": "intraday",
-                "latest_sla_miss_state": True
-            },
-            False,
-        ),
-        (
-            {
-                "sla_interval": "3h",
-                "sla_time": sla_time(-1),
-                "max_execution_date": hours_from_now(-3),
-                "cadence": "intraday",
-                "latest_sla_miss_state": False
-            },
-            False,
-        ),
-        (
-            {
-                "sla_interval": "1h",
-                "sla_time": None,
-                "max_execution_date": hours_from_now(-2),
-                "cadence": "triggered",
-                "latest_sla_miss_state": True
+                "latest_sla_miss_state": False,
             },
             True,
         ),
         (
+            # 5. max_execution_date -> checkpoint -> now
             {
-                "sla_interval": "1h",
-                "sla_time": None,
-                "max_execution_date": hours_from_now(-2),
+                "sla_interval": "2h",
+                "sla_time": sla_time(-1),
+                "max_execution_date": hours_from_now(-4),
                 "cadence": "triggered",
-                "latest_sla_miss_state": False
+                "latest_sla_miss_state": None,
             },
             True,
         ),
         (
+            # 6. checkpoint -> max_execution_date -> now
             {
-                "sla_interval": "1h",
-                "sla_time": None,
-                "max_execution_date": hours_from_now(-2),
+                "sla_interval": "2h",
+                "sla_time": sla_time(-1),
+                "max_execution_date": hours_from_now(-1),
                 "cadence": "intraday",
-                "latest_sla_miss_state": True
+                "latest_sla_miss_state": True,
+            },
+            False,
+        ),
+        (
+            # 7. checkpoint -> max_execution_date -> now
+            {
+                "sla_interval": "2h",
+                "sla_time": sla_time(-1),
+                "max_execution_date": hours_from_now(-1),
+                "cadence": "intraday",
+                "latest_sla_miss_state": False,
+            },
+            False,
+        ),
+        (
+            # 8. checkpoint -> max_execution_date -> now
+            {
+                "sla_interval": "2h",
+                "sla_time": sla_time(-1),
+                "max_execution_date": hours_from_now(-1),
+                "cadence": "intraday",
+                "latest_sla_miss_state": None,
+            },
+            False,
+        ),
+        (
+            # 9. checkpoint -> max_execution_date -> now
+            {
+                "sla_interval": "2h",
+                "sla_time": sla_time(-1),
+                "max_execution_date": hours_from_now(-1),
+                "cadence": "triggered",
+                "latest_sla_miss_state": True,
+            },
+            False,
+        ),
+        (
+            # 10. checkpoint -> max_execution_date -> now
+            {
+                "sla_interval": "2h",
+                "sla_time": sla_time(-1),
+                "max_execution_date": hours_from_now(-1),
+                "cadence": "triggered",
+                "latest_sla_miss_state": False,
+            },
+            False,
+        ),
+        (
+            # 11. checkpoint -> max_execution_date -> now
+            {
+                "sla_interval": "2h",
+                "sla_time": sla_time(-1),
+                "max_execution_date": hours_from_now(-1),
+                "cadence": "triggered",
+                "latest_sla_miss_state": None,
+            },
+            False,
+        ),
+        (
+            # 12. max_execution_date -> checkpoint -> now
+            {
+                "sla_interval": "2h",
+                "sla_time": None,
+                "max_execution_date": hours_from_now(-4),
+                "cadence": "intraday",
+                "latest_sla_miss_state": True,
             },
             True,
         ),
         (
+            # 13. max_execution_date -> checkpoint -> now
             {
-                "sla_interval": "1h",
+                "sla_interval": "2h",
                 "sla_time": None,
-                "max_execution_date": hours_from_now(-2),
+                "max_execution_date": hours_from_now(-4),
                 "cadence": "intraday",
-                "latest_sla_miss_state": False
+                "latest_sla_miss_state": False,
             },
             True,
         ),
         (
+            # 14. max_execution_date -> checkpoint -> now
             {
-                "sla_interval": "3h",
+                "sla_interval": "2h",
                 "sla_time": None,
-                "max_execution_date": hours_from_now(-2),
-                "cadence": "triggered",
-                "latest_sla_miss_state": True
-            },
-            False,
-        ),
-        (
-            {
-                "sla_interval": "3h",
-                "sla_time": None,
-                "max_execution_date": hours_from_now(-2),
-                "cadence": "triggered",
-                "latest_sla_miss_state": False
-            },
-            False,
-        ),
-        (
-            {
-                "sla_interval": "3h",
-                "sla_time": None,
-                "max_execution_date": hours_from_now(-2),
+                "max_execution_date": hours_from_now(-4),
                 "cadence": "intraday",
-                "latest_sla_miss_state": True
+                "latest_sla_miss_state": None,
             },
-            False,
+            True,
         ),
         (
+            # 15. max_execution_date -> checkpoint -> now
             {
-                "sla_interval": "3h",
+                "sla_interval": "2h",
                 "sla_time": None,
-                "max_execution_date": hours_from_now(-2),
+                "max_execution_date": hours_from_now(-4),
+                "cadence": "triggered",
+                "latest_sla_miss_state": True,
+            },
+            True,
+        ),
+        (
+            # 16. max_execution_date -> checkpoint -> now
+            {
+                "sla_interval": "2h",
+                "sla_time": None,
+                "max_execution_date": hours_from_now(-4),
+                "cadence": "triggered",
+                "latest_sla_miss_state": False,
+            },
+            True,
+        ),
+        (
+            # 17. max_execution_date -> checkpoint -> now
+            {
+                "sla_interval": "2h",
+                "sla_time": None,
+                "max_execution_date": hours_from_now(-4),
+                "cadence": "triggered",
+                "latest_sla_miss_state": None,
+            },
+            True,
+        ),
+        (
+            # 18. checkpoint -> max_execution_date -> now
+            {
+                "sla_interval": "2h",
+                "sla_time": None,
+                "max_execution_date": hours_from_now(-1),
                 "cadence": "intraday",
-                "latest_sla_miss_state": False
+                "latest_sla_miss_state": True,
             },
             False,
         ),
         (
+            # 19. checkpoint -> max_execution_date -> now
             {
-                "sla_interval": "3h",
+                "sla_interval": "2h",
+                "sla_time": None,
+                "max_execution_date": hours_from_now(-1),
+                "cadence": "intraday",
+                "latest_sla_miss_state": False,
+            },
+            False,
+        ),
+        (
+            # 20. checkpoint -> max_execution_date -> now
+            {
+                "sla_interval": "2h",
+                "sla_time": None,
+                "max_execution_date": hours_from_now(-1),
+                "cadence": "intraday",
+                "latest_sla_miss_state": None,
+            },
+            False,
+        ),
+        (
+            # 21. checkpoint -> max_execution_date -> now
+            {
+                "sla_interval": "2h",
+                "sla_time": None,
+                "max_execution_date": hours_from_now(-1),
+                "cadence": "triggered",
+                "latest_sla_miss_state": True,
+            },
+            False,
+        ),
+        (
+            # 22. checkpoint -> max_execution_date -> now
+            {
+                "sla_interval": "2h",
+                "sla_time": None,
+                "max_execution_date": hours_from_now(-1),
+                "cadence": "triggered",
+                "latest_sla_miss_state": False,
+            },
+            False,
+        ),
+        (
+            # 23. checkpoint -> max_execution_date -> now
+            {
+                "sla_interval": "2h",
+                "sla_time": None,
+                "max_execution_date": hours_from_now(-1),
+                "cadence": "triggered",
+                "latest_sla_miss_state": None,
+            },
+            False,
+        ),
+        #Group 2. 
+        #    - Before SLA time and not triggered DAG
+        #    - return latest_sla_miss_state or False
+        (
+            # 24.
+            {
+                "sla_interval": "2h",
                 "sla_time": sla_time(1),
-                "max_execution_date": hours_from_now(-3),
-                "cadence": "triggered",
-                "latest_sla_miss_state": True
-            },
-            False,
-        ),
-        (
-            {
-                "sla_interval": "3h",
-                "sla_time": sla_time(1),
-                "max_execution_date": hours_from_now(-3),
-                "cadence": "triggered",
-                "latest_sla_miss_state": False
-            },
-            False,
-        ),
-        (
-            {
-                "sla_interval": "3h",
-                "sla_time": sla_time(1),
-                "max_execution_date": hours_from_now(-3),
+                "max_execution_date": hours_from_now(-4),
                 "cadence": "intraday",
-                "latest_sla_miss_state": True
+                "latest_sla_miss_state": True,
             },
             True,
         ),
         (
+            # 25.
             {
-                "sla_interval": "3h",
+                "sla_interval": "2h",
                 "sla_time": sla_time(1),
-                "max_execution_date": hours_from_now(-3),
+                "max_execution_date": hours_from_now(-4),
                 "cadence": "intraday",
-                "latest_sla_miss_state": False
+                "latest_sla_miss_state": False,
             },
             False,
         ),
-    ]
+        (
+            # 26. First check on a new alert
+            {
+                "sla_interval": "2h",
+                "sla_time": sla_time(1),
+                "max_execution_date": hours_from_now(-4),
+                "cadence": "intraday",
+                "latest_sla_miss_state": None,
+            },
+            False,
+        ),
+        (
+            # 27.
+            {
+                "sla_interval": "2h",
+                "sla_time": sla_time(1),
+                "max_execution_date": hours_from_now(-1),
+                "cadence": "intraday",
+                "latest_sla_miss_state": True,
+            },
+            True,
+        ),
+        (
+            # 28.
+            {
+                "sla_interval": "2h",
+                "sla_time": sla_time(1),
+                "max_execution_date": hours_from_now(-1),
+                "cadence": "intraday",
+                "latest_sla_miss_state": False,
+            },
+            False,
+        ),
+        (
+            # 29. First check on a new alert
+            {
+                "sla_interval": "2h",
+                "sla_time": sla_time(1),
+                "max_execution_date": hours_from_now(-1),
+                "cadence": "intraday",
+                "latest_sla_miss_state": None,
+            },
+            False,
+        ),
+        #Group 3. 
+        #    - Everything else
+        #    - return False
+        (
+            # 30.
+            {
+                "sla_interval": "2h",
+                "sla_time": sla_time(1),
+                "max_execution_date": hours_from_now(-4),
+                "cadence": "triggered",
+                "latest_sla_miss_state": True,
+            },
+            False,
+        ),
+        (
+            # 31.
+            {
+                "sla_interval": "2h",
+                "sla_time": sla_time(1),
+                "max_execution_date": hours_from_now(-4),
+                "cadence": "triggered",
+                "latest_sla_miss_state": False,
+            },
+            False,
+        ),
+        (
+            # 32.
+            {
+                "sla_interval": "2h",
+                "sla_time": sla_time(1),
+                "max_execution_date": hours_from_now(-4),
+                "cadence": "triggered",
+                "latest_sla_miss_state": None,
+            },
+            False,
+        ),
+        (
+            # 33.
+            {
+                "sla_interval": "2h",
+                "sla_time": sla_time(1),
+                "max_execution_date": hours_from_now(-1),
+                "cadence": "triggered",
+                "latest_sla_miss_state": True,
+            },
+            False,
+        ),
+        (
+            # 34.
+            {
+                "sla_interval": "2h",
+                "sla_time": sla_time(1),
+                "max_execution_date": hours_from_now(-1),
+                "cadence": "triggered",
+                "latest_sla_miss_state": False,
+            },
+            False,
+        ),
+        (
+            # 35.
+            {
+                "sla_interval": "2h",
+                "sla_time": sla_time(1),
+                "max_execution_date": hours_from_now(-1),
+                "cadence": "triggered",
+                "latest_sla_miss_state": None,
+            },
+            False,
+        ),
+    ],
 )
 def test_sla_check(sla_check_kwargs, expected_result):
     assert sla_check(**sla_check_kwargs) == expected_result
-
